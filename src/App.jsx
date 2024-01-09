efa01247253a0cda9568f4491da474e59a3871b8
Saikat Das <saikatdas40g@gmail.com>
2024-01-09 12:23:50 +0530

    design updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import "./App.css";
 
 function App() {
   const [username, setUsername] = useState("");
@@ -15,60 +16,81 @@ function App() {
       password,
     };
     const requestOptions = {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(data),
     };
     try {
       const response = await fetch(
-        "https://backendfortodo-production.up.railway.app/api/v1/users/register",
+        "http://localhost:8000/api/v1/users/register",
         requestOptions
       );
       console.log("response done", response);
-      if(!response.ok) throw new Error('Network response not ok');
+      if (!response.ok) throw new Error("Network response not ok");
       const responseData = await response.json();
       console.log("responseData", responseData);
     } catch (error) {
-      console.error("mongo error",error.message);
+      console.error("mongo error", error.message);
     }
   };
 
   return (
-    <>
-      <form method="post">
-        <input
-          type="text"
-          value={username}
-          onChange={(e) => {
-            setUsername(e.target.value);
-          }}
-        />
-        <input
-          type="text"
-          value={email}
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-        />
-        <input
-          type="text"
-          value={password}
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
-        <input
-          type="text"
-          value={fullName}
-          onChange={(e) => {
-            setFullname(e.target.value);
-          }}
-        />
-        <button onClick={handleSubmit}>submit</button>
+    <div className="img-container">
+      <img
+        src="/pexels-sebastian-coman-photography-3461205.jpg"
+        className="cover-photo"
+      />
+      <form method="post" className="form">
+        <h1>Register yourself!</h1>
+        <div className="formbody">
+          <div className="name">
+            <p>Name</p>
+            <input
+              type="text"
+              value={fullName}
+              onChange={(e) => {
+                setFullname(e.target.value);
+              }}
+            />
+          </div>
+          <div className="email">
+            <p>Email</p>
+            <input
+              type="text"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+          </div>
+          <div className="username">
+            <p>Username</p>
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => {
+                setUsername(e.target.value);
+              }}
+            />
+          </div>
+          <div className="password">
+            <p>Password</p>
+            <input
+              type="text"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+          </div>
+          <div className="submit">
+            <button onClick={handleSubmit}>Submit</button>
+          </div>
+        </div>
       </form>
-    </>
+    </div>
   );
 }
 
