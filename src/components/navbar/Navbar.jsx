2265d89f807dd818e15ec2ee490ae955e23493c0
Saikat Das <saikatdas40g@gmail.com>
2024-01-14 19:19:39 +0530

    changes in backeend

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./navbar.css";
 import { Link } from "react-router-dom";
 function Navbar() {
@@ -7,24 +7,52 @@ function Navbar() {
     headers: {
       "Content-Type": "application/json",
     },
+    credentials: "include",
   };
-  let res;
-  const load = async () => {
-    res = await fetch(
-      "http://localhost:8000/api/v1/users/getCurrentuser",
-      requestOptions
-    );
-    console.log(res);
-  };
-  // load();
-  if (!res) {
+
+  const localServer = `${
+    import.meta.env.VITE_LOCALHOST_SERVER_LINK
+  }/users/login`;
+  const hostedServer = `${import.meta.env.VITE_HOSTED_SERVER_LINK}/users/login`;
+
+  const [userData, setUserData] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          import.meta.env.VITE_DEVELOPMENT_ENV === "true"
+            ? localServer
+            : hostedServer,
+          requestOptions
+        );
+
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        setUserData(data);
+      } catch (error) {
+        setError(error.message);
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(userData);
+
+  if (!userData) {
     return (
       <div className="navBody fixed w-full flex justify-between items-center bg-zinc-950 bg-opacity-40 backdrop-blur-3xl text-white p-5">
         <div className="logoName">Todo</div>
         <div className="menu">
           <ul className="flex gap-4">
             <li>
-              <Link to="/login">Login</Link>
+              <Link to="/login">login</Link>
             </li>
             <li>
               <Link to="/signup">Signup</Link>
@@ -33,15 +61,28 @@ function Navbar() {
         </div>
       </div>
     );
-  } else return (
+  }
+  return (
     <div className="navBody fixed w-full flex justify-between items-center bg-zinc-950 bg-opacity-40 backdrop-blur-3xl text-white p-5">
-        <div className="logoName">Todo</div>
-        <div className="menu">
-          <ul className="flex gap-4">
-            <li>Hello</li>
-          </ul>
-        </div>
+      <div className="logoName">Todo</div>
+      <div className="menu">
+        <ul className="flex gap-4">
+          <li>
+            <Link to="/addTodo">Add Todo</Link>
+          </li>
+          <li>
+            <Link to="/allTodos">All Todo</Link>
+          </li>
+          <li>
+            <Link to="/search">Search</Link>
+          </li>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+          <li>hello {userData.data.fullName}</li>
+        </ul>
       </div>
+    </div>
   );
 }
 
