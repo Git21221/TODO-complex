d827ec582e4d2ba16f7c5423c24e322daed9e977
Saikat Das <saikatdas40g@gmail.com>
2024-02-20 21:00:23 +0530

    cors

--- server/src/app.js
+++ server/src/app.js
@@ -10,7 +10,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CORS_ORIGIN,
     credentials: true,
   })
 );
