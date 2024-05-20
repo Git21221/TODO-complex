610d81f12f44228d938d5afef4f1f8fa89ba6d51
Saikat Das <saikatdas40g@gmail.com>
2024-05-20 23:36:32 +0530

    fix: Update multer middleware destination path

--- server/src/middlewares/multer.middleware.js
+++ server/src/middlewares/multer.middleware.js
@@ -2,7 +2,7 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "/public");
+    cb(null, "./");
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
